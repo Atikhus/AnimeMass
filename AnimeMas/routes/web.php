a7b911fcd49a7f4316977127af27f8905ce8a021
luis dev <luismiguel202016@gmail.com>
2024-09-23 20:07:29 -0500

    se agrego un controlador y los assets css y el index de la pagina

--- AnimeMas/routes/web.php
+++ AnimeMas/routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use app\Http\Controllers\CategoriaController;
+use Illuminate\Routing\RouteParameterBinder;
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+ */
+
+Route::get('/',[CategoriaController::class,'index']);
+
+Route::get('/', function () {
+    return view('index');
+});
+
