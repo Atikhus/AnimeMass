e83e44f6e4dbb7b9fab6d7ac70251952b6f6127d
luis dev <luismiguel202016@gmail.com>
2024-10-31 12:49:03 -0500

    se personalizo el gmail que se envia al usuario

---
+++ lang/en/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | outcome such as failure due to an invalid password / reset token.
+    |
+    */
+
+    'reset' => '¡Tu contraseña ha sido restablecida!',
+    'sent' => 'Anime mas: ¡Te hemos enviado por correo el enlace para restablecer tu contraseña!.',
+    'throttled' => 'Por favor espera antes de intentar de nuevo.',
+    'token' => 'Este token de restablecimiento de contraseña no es válido.',
+    'user' => "No podemos encontrar un usuario con esa dirección de correo electrónico.",
+
+];
