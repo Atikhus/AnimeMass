e26ff93ae09f576f0f3baed52da161202de22521
luis dev <luismiguel202016@gmail.com>
2024-11-04 15:14:11 -0500

    se agrego el instructivo

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -22,7 +22,6 @@ return new class extends Migration
             $table->timestamps();
         });
 
-
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
             $table->string('token');
